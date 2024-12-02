b359ecb889627f2a334196fc7b5b017b3a78dd26
naxosit <128820710+naxosit@users.noreply.github.com>
2024-12-01 23:53:56 -0300

    Se añade la petición de acceso/ejecución

--- fragmentacion_vertical.py
+++ fragmentacion_vertical.py
@@ -14,7 +14,7 @@ def solicitar_matriz(nombre):
     return np.array(matriz)
 
 # Calcular la matriz de afinidad
-def calcular_matriz_afinidad(query_attr, query_access):
+def calcular_matriz_afinidad(query_attr, query_access, acceso_ejecucion):
     num_atributos = query_attr.shape[1]
     aa_matrix = np.zeros((num_atributos, num_atributos))
     
@@ -22,22 +22,27 @@ def calcular_matriz_afinidad(query_attr, query_access):
         for j in range(num_atributos):
             if i == j:  # Diagonal principal: afinidad de un atributo consigo mismo
                 consultas_con_attr = np.where(query_attr[:, i] == 1)[0]
-                aa_matrix[i, j] = np.sum(query_access[consultas_con_attr, :])
+                aa_matrix[i, j] = np.sum(query_access[consultas_con_attr, :] * acceso_ejecucion)
             else:  # Afinidad entre atributos distintos
                 consultas_con_both_attrs = np.where((query_attr[:, i] == 1) & (query_attr[:, j] == 1))[0]
-                aa_matrix[i, j] = np.sum(query_access[consultas_con_both_attrs, :])
+                aa_matrix[i, j] = np.sum(query_access[consultas_con_both_attrs, :] * acceso_ejecucion)
     
     return aa_matrix
 
 # Main program para generar las matrices
 if __name__ == "__main__":
     # Solicitar matrices de uso y acceso
+    print("\nPor favor, ingrese la matriz de uso de atributos (query_attr):")
     query_attr = solicitar_matriz("de uso")
 
+    print("\nPor favor, ingrese la matriz de acceso por consultas y sitios (query_access):")
     query_access = solicitar_matriz("de acceso")
 
+    # Solicitar el valor de acceso/ejecución
+    acceso_ejecucion = float(input("\nIngrese el valor de acceso/ejecución: "))
+
     # Generar la matriz de afinidad
-    aa_matrix = calcular_matriz_afinidad(query_attr, query_access)
+    aa_matrix = calcular_matriz_afinidad(query_attr, query_access, acceso_ejecucion)
 
     print("\nMatriz de Afinidad (AA) generada:")
     print(aa_matrix)
